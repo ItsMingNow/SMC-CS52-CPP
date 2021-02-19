786a882f4392e91f5d2fc2a4b6d35fa761b9f825
Yanming Yu <69600884+jimmyjameswong@users.noreply.github.com>
2021-02-18 17:06:55 -0800

    peas in a pod program

--- peasInAPod.cpp
+++ peasInAPod.cpp
@@ -5,4 +5,19 @@ int main()
 {
     int numberOfPods, peasPerPod, totalPeas;
     cout << "Press return after entering a number.\n";
+    cout << "Enter number of pods:\n";
+    cin >> numberOfPods;
+    cout << "Enter the number of peas in a pod:\n";
+    cin >> peasPerPod;
+    totalPeas = numberOfPods * peasPerPod;
+    cout << "If you have ";
+    cout << numberOfPods;
+    cout << " pea pods\n";
+    cout << "and ";
+    cout << peasPerPod;
+    cout << " peas in each pod, then\n";
+    cout << "you have ";
+    cout << totalPeas;
+    cout << " peas in all the pods.\n";
+    return 0;
 }
